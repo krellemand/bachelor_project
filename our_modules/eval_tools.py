8c5e510dd845f83697cb61fe3360b686d4f5cd46
krellemand <krellemand@gmail.com>
2023-03-28 14:20:38 +0200

    Finished plotting goldfish fn fgsm and eps

--- our_modules/eval_tools.py
+++ our_modules/eval_tools.py
@@ -31,7 +31,7 @@ def mls_osr_score(logits):
 def get_osr_targets(csr_targets, split_targets):
     return (~(sum(csr_targets == i for i in split_targets).bool()))
 
-def load_and_eval_mls_osr(logit_file_path, csr_targets_file_path, split_num, dataset_name='tinyimagenet', balance=False):
+def load_and_eval_mls_osr(logit_file_path, csr_targets_file_path, split_num, dataset_name='tinyimagenet', balance=False, return_avg_mls=False):
     assert int(logit_file_path[-4]) == split_num, "The split_num does not correspond to the split num of the file name"
     split = osr_splits[dataset_name][split_num]
     logits = torch.load(logit_file_path)
@@ -41,22 +41,37 @@ def load_and_eval_mls_osr(logit_file_path, csr_targets_file_path, split_num, dat
     # print((sum(open_set_labels)/len(open_set_labels)).data) # Balance ratio before
     if balance:
         mls_oslabel = balance_binary(zip(mls_scores.tolist(), open_set_labels.tolist()), lambda x: bool(x[1]))
-        mls_scores, open_set_labels = ([mls for mls, _ in mls_oslabel ], 
+        mls_scores, open_set_labels = (torch.tensor([mls for mls, _ in mls_oslabel ]), 
                                        [oslabel for _, oslabel in mls_oslabel])
     # print(sum(open_set_labels)/len(open_set_labels)) # Balance ratio after
+    if return_avg_mls:
+        return evaluate_osr(mls_scores, open_set_labels), -torch.mean(mls_scores)
     return evaluate_osr(mls_scores, open_set_labels)
 
-def load_and_eval_mls_osr_for_all_eps(path_to_eps_dirs, split_num, dataset_name='tinyimagenet', balance=False):
+def load_and_eval_mls_osr_for_all_eps(path_to_eps_dirs, split_num, dataset_name='tinyimagenet', balance=False, return_avg_mls=False):
     eps_dir_list = [dir_name for dir_name in os.listdir(path_to_eps_dirs) if dir_name[:3] == 'eps']
     eps_list = [float(dir_name[4:]) for dir_name in eps_dir_list]
     roc_stats = []
+    avg_mls_list = []
     for dir in eps_dir_list:
-        roc_stat_tuple = load_and_eval_mls_osr(path_to_eps_dirs + dir + '/logits_' + str(split_num) + '.pt',
+        stats = load_and_eval_mls_osr(path_to_eps_dirs + dir + '/logits_' + str(split_num) + '.pt',
                                                path_to_eps_dirs + dir + '/csr_targets_' + str(split_num) + '.pt',
                                                split_num=split_num,
                                                dataset_name=dataset_name,
-                                               balance=balance)
+                                               balance=balance,
+                                               return_avg_mls=return_avg_mls)
+        if return_avg_mls:
+            roc_stat_tuple, avg_mls = stats
+            avg_mls_list += avg_mls,
+        else:
+            roc_stat_tuple = stats
         roc_stats += roc_stat_tuple,
+    if return_avg_mls:
+        eps_roc_mls = sorted(zip(eps_list, roc_stats, avg_mls_list), key=lambda x: x[0])
+        eps_list = [eps for eps, _, _ in eps_roc_mls]
+        roc_stats = [roc_stat for _, roc_stat, _ in eps_roc_mls]
+        avg_mls_list = [mls for _, _, mls in eps_roc_mls]
+        return eps_list, roc_stats, avg_mls_list
     eps_roc = sorted(zip(eps_list, roc_stats), key=lambda x: x[0])
     eps_list = [eps for eps, _ in eps_roc]
     roc_stats = [roc_stat for _, roc_stat in eps_roc]
