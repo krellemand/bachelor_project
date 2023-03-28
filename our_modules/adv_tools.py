ed1c950fe159521bd852210866b39acbb2c70abf
Drilip <gks462@alumni.ku.dk>
2023-03-28 11:12:32 +0200

    Added images showing adv attack impact.

--- our_modules/adv_tools.py
+++ our_modules/adv_tools.py
@@ -1,25 +1,35 @@
 import torch
 
-def fgsm(model, xs, ys, eps, loss_func, clip_range=(None, None)):
+def fgsm(model, xs, ys, eps, loss_func, clip_range=(None, None), return_step=False):
     if len(xs.shape) == 3:
         xs.requires_grad = True
         loss = loss_func(model(xs), ys)
         loss.backward()
-        return torch.clip(xs + eps*torch.sign(xs.grad), clip_range[0], clip_range[1])
+        step = eps*torch.sign(xs.grad)
+        if return_step:
+            return torch.clip(xs + step, clip_range[0], clip_range[1]), step
+        return torch.clip(xs + step, clip_range[0], clip_range[1])
     elif len(xs.shape) == 4:
         output = []
+        steps = []
         for x, y in zip(xs,ys):
             x.requires_grad = True
             loss = loss_func(model(x[None]), y)
             loss.backward()
-            output.append((x + eps*torch.sign(x.grad))[None])
+            step = eps*torch.sign(x.grad)
+            steps.append(step[None])
+            output.append((x + step)[None])
+        if return_step:
+            return torch.clip(torch.cat(output), clip_range[0], clip_range[1]), torch.cat(steps)
         return torch.clip(torch.cat(output), clip_range[0], clip_range[1])
     else:
         raise NotImplementedError
     
+def fp_osr_fgsm(model, x, eps=0.05, clip_range=(None, None), return_step=False):
+    return fgsm(model, x, torch.zeros(len(x)), -eps, lambda y_hat, y: torch.linalg.norm(y_hat, dim=-1, ord=None), 
+                clip_range=clip_range, return_step=return_step)
 
-def fp_osr_fgsm(model, x, eps=0.05, clip_range=(None, None)):
-    return fgsm(model, x, torch.zeros(len(x)), -eps, lambda y_hat, y: torch.linalg.norm(y_hat, dim=-1, ord=None), clip_range=clip_range)
+def fn_osr_fgsm(model, x, eps=0.05, clip_range=(None, None), return_step=False):
+    return fgsm(model, x, torch.zeros(len(x)), eps, lambda y_hat, y: torch.linalg.norm(y_hat, dim=-1, ord=torch.inf), 
+                clip_range=clip_range, return_step=return_step)
 
-def fn_osr_fgsm(model, x, eps=0.05, clip_range=(None, None)):
-    return fgsm(model, x, torch.zeros(len(x)), eps, lambda y_hat, y: torch.linalg.norm(y_hat, dim=-1, ord=torch.inf), clip_range=clip_range)
--- our_modules/plot_tools.py
+++ our_modules/plot_tools.py
@@ -1,5 +1,27 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_roc(ax, roc_stats, **plt_kwargs):
     fprs, tprs, thresholds = roc_stats
     ax.plot(fprs, tprs, **plt_kwargs)
+
+def plot_image_on_ax(ax, normalized_img, mean, std, **plt_kwargs):
+    normalized_img = normalized_img.to('cpu')
+    if normalized_img.requires_grad:
+        normalized_img = normalized_img.detach()
+    img = normalized_img*np.array(std)[:, None, None] + np.array(mean)[:, None, None]
+    ax.imshow(np.clip(img.permute(1,2,0).numpy(), 0.0, 1.0), **plt_kwargs)
+
+def plot_image(normalized_img, mean, std, **plt_kwargs):
+    fig, ax = plt.subplots(1,1, figsize=(5,5))
+    plot_image_on_ax(ax, normalized_img.to('cpu'), mean=mean, std=std, **plt_kwargs)
+    plt.show()
+
+def plot_image_i(i, dataset, mean, std, **plt_kwargs):
+    img, label, uq_idx = dataset[i]
+    # Unnormalize
+    img = img*np.array(std)[:, None, None] + np.array(mean)[:, None, None]
+    # The image is clipped due to numerical imprecision.
+    plt.imshow(np.clip(img.permute(1,2,0).numpy(), 0.0, 1.0), **plt_kwargs)
+    print(label, uq_idx)
+    plt.show()
