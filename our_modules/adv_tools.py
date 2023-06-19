b4c27893c95aa748a48ba9c1de5f9341d9825445
krellemand <krellemand@gmail.com>
2023-06-19 13:05:34 +0200

    Added comments

--- our_modules/adv_tools.py
+++ our_modules/adv_tools.py
@@ -1,22 +1,46 @@
 import torch
 import os
 
+# These are the different loss funcitons used in the advesarial attacks. There reasoning behind the different losses is described in the Methodology chapter of our paper.
+
 def log_msp_loss(y_hat, y):
+    """Log Maximum Softmax Probablity Loss"""
     return torch.log(torch.amax(torch.exp(y_hat), dim=-1)/torch.sum(torch.exp(y_hat), dim=-1))
 
+
 def norm_loss(y_hat, y, **norm_kwargs):
+    """Used to create the 2 norm Loss"""
     return torch.linalg.norm(y_hat, **norm_kwargs)
 
+
 def sum_exp_loss(y_hat, y, dim=-1):
+    """Sum of Exponetials Loss"""
     return torch.sum(torch.exp(y_hat), dim=dim)
 
+
 def max_loss(y_hat, y, dim=-1):
+    """The max loss"""
     return torch.amax(y_hat, dim=dim)
 
+
 def iterative_attack(model, xs, ys, loss_func, torch_optim, clip_range=(None, None), eps=None, return_step=False, max_iter=100, threshold=None, adv_type='fn', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
+    """
+    Creates an Advesararial Example using a itterative method for the inputs "xs" that influses the model "model" prediction.
+    The true labels "ys" are only used if the loss function is dependent on the labels of the images.
+
+    loss_func:      The loss funciton that is maximized
+    clip_range:     The range for pixel values. This is calulated based on the normilization used in preeprossesing. This is very dependent on the dataset.
+    eps:            The range for how much a single pixel value can change
+    return_step:    Weather the pertubation is returned seprately.
+    max_iter:       Number of iterations
+    threshold:      Weather the model should stop for a this given threshold.
+    adv_type:       The sype of attack only used for thresholds.
+    score_func:     The scoring funciton is also only used for if threshold is wanted with a given scoring function.
+    """
     device = xs.device
     if clip_range != (None, None):
         clip_range = tuple([t.to(device) for t in clip_range])
+
     if len(xs.shape) == 3:
         x = xs
         y = ys
@@ -24,6 +48,7 @@ def iterative_attack(model, xs, ys, loss_func, torch_optim, clip_range=(None, No
         x_init = torch.clone(x).detach()
         optimizer = torch_optim([x], **opt_kwargs)
         i = 0
+
         while i < max_iter:
             optimizer.zero_grad()
             logits = model(x[None])
@@ -46,11 +71,12 @@ def iterative_attack(model, xs, ys, loss_func, torch_optim, clip_range=(None, No
                 x.copy_(torch.clip(x, min=clip_range[0], max=clip_range[1]))
             x.requires_grad = True
             i += 1
-        # print(f'Ran {i} of {max_iter} possible iterations')
+
         if return_step:
             step = x - x_init
             return x, step
         return x
+    
     elif len(xs.shape) == 4:
         output = []
         steps = []
@@ -60,10 +86,12 @@ def iterative_attack(model, xs, ys, loss_func, torch_optim, clip_range=(None, No
             x_init = torch.clone(x).detach()
             optimizer = torch_optim([x], **opt_kwargs)
             i = 0
+
             while i < max_iter:
                 optimizer.zero_grad()
                 logits = model(x[None])
                 loss = loss_func(logits, y)
+
                 if threshold is not None:
                     score = score_func(logits)
                     if adv_type == 'fn':
@@ -74,38 +102,61 @@ def iterative_attack(model, xs, ys, loss_func, torch_optim, clip_range=(None, No
                         raise NotImplementedError
                     if to_stop:
                         break
+
                 loss.backward()
                 optimizer.step()
+
                 with torch.no_grad():
                     if eps is not None:
                         x.copy_(torch.clip(x, min=x_init - eps*torch.ones_like(x_init), max=x_init + eps*torch.ones_like(x_init)))    
                     x.copy_(torch.clip(x, min=clip_range[0], max=clip_range[1]))
+
                 x.requires_grad = True
                 i += 1
+
             step = x - x_init
             output.append(x[None])
             steps.append(step[None])
-        # print(f'Ran {i} of {max_iter} possible iterations')
+
         if return_step:
             return torch.cat(output), torch.cat(steps)
         return torch.cat(output)
 
+
+# We are here creating iterative attacks for each type of attack. FP = False Posetive and FN = False Negative
+
 def fp_osr_itat(model, x, eps=0.05, clip_range=(None, None), return_step=False, norm_ord=None, max_iter=25, torch_opt=torch.optim.Rprop, threshold=None, adv_type='fp', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
     return iterative_attack(model, x, torch.zeros(len(x)), lambda yhat, y: norm_loss(yhat, y, ord=norm_ord, dim=-1), torch_opt, clip_range=clip_range, eps=eps, return_step=return_step, max_iter=max_iter, threshold=threshold, adv_type=adv_type, score_func=score_func, **opt_kwargs)  
+
 
 def fn_osr_itat(model, x, eps=0.05, clip_range=(None, None), return_step=False, norm_ord=None, max_iter=25, torch_opt=torch.optim.Rprop, threshold=None, adv_type='fn', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
     return iterative_attack(model, x, torch.zeros(len(x)), lambda yhat, y: -norm_loss(yhat, y, ord=norm_ord, dim=-1), torch_opt, clip_range=clip_range, eps=eps, return_step=return_step, max_iter=max_iter, threshold=threshold, adv_type=adv_type, score_func=score_func, **opt_kwargs)  
 
+
 def fp_osr_itat_max(model, x, eps=0.05, clip_range=(None, None), return_step=False, max_iter=25, torch_opt=torch.optim.Rprop, threshold=None, adv_type='fp', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
     return iterative_attack(model, x, torch.zeros(len(x)), lambda yhat, y: max_loss(yhat, y, dim=-1), torch_opt, clip_range=clip_range, eps=eps, return_step=return_step, max_iter=max_iter, threshold=threshold, adv_type=adv_type, score_func=score_func, **opt_kwargs)  
+
 
 def fn_osr_itat_max(model, x, eps=0.05, clip_range=(None, None), return_step=False, max_iter=25, torch_opt=torch.optim.Rprop, threshold=None, adv_type='fn', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
     return iterative_attack(model, x, torch.zeros(len(x)), lambda yhat, y: -max_loss(yhat, y, dim=-1), torch_opt, clip_range=clip_range, eps=eps, return_step=return_step, max_iter=max_iter, threshold=threshold, adv_type=adv_type, score_func=score_func, **opt_kwargs)
 
+
 def fp_osr_itat_sum_exp(model, x, eps=0.05, clip_range=(None, None), return_step=False, max_iter=25, torch_opt=torch.optim.Rprop, threshold=None, adv_type='fp', score_func= lambda logits: torch.amax(logits, dim=-1), **opt_kwargs):
     return iterative_attack(model, x, torch.zeros(len(x)), lambda yhat, y: sum_exp_loss(yhat, y, dim=-1), torch_opt, clip_range=clip_range, eps=eps, return_step=return_step, max_iter=max_iter, threshold=threshold, adv_type=adv_type, score_func=score_func, **opt_kwargs) 
 
+
+
 def fgsm(model, xs, ys, eps, loss_func, clip_range=(None, None), return_step=False, **loss_kwargs):
+    """
+    Creates an Advesararial Example with the fgsm for the inputs "xs" that influses the models "model" prediction.
+    The true labels "ys" are only used if the loss function is dependent on the labels of the images.
+    
+    loss_func:      The loss funciton that is maximized
+    clip_range:     The range for pixel values. This is calulated based on the normilization used in preeprossesing. This is very dependent on the dataset.
+    eps:            The range for how much a single pixel value can change
+    return_step:    Weather the pertubation is returned seprately.
+    loss_kwargs:    Extra inputs for a loss function
+    """
     device = xs.device
     if clip_range != (None, None):
         clip_range = tuple([t.to(device) for t in clip_range])
@@ -117,9 +168,11 @@ def fgsm(model, xs, ys, eps, loss_func, clip_range=(None, None), return_step=Fal
         if return_step:
             return torch.clip(xs + step, clip_range[0], clip_range[1]), step
         return torch.clip(xs + step, clip_range[0], clip_range[1])
+    
     elif len(xs.shape) == 4:
         output = []
         steps = []
+
         for x, y in zip(xs,ys):
             x.requires_grad = True
             loss = loss_func(model(x[None]), y, **loss_kwargs)
@@ -127,22 +180,30 @@ def fgsm(model, xs, ys, eps, loss_func, clip_range=(None, None), return_step=Fal
             step = eps*torch.sign(x.grad)
             steps.append(step[None])
             output.append((x + step)[None])
+
         if return_step:
             return torch.clip(torch.cat(output), clip_range[0], clip_range[1]), torch.cat(steps)
         return torch.clip(torch.cat(output), clip_range[0], clip_range[1])
+    
     else:
         raise NotImplementedError
+
+
+# We are here creating single step attacks using the fast gradient sign method for each type of attack. FP = False Posetive and FN = False Negative
 
 def fp_osr_fgsm(model, x, eps=0.05, clip_range=(None, None), return_step=False, norm_ord=None):
     return fgsm(model, x, torch.zeros(len(x)), -eps, norm_loss, dim=-1, ord=norm_ord, 
                 clip_range=clip_range, return_step=return_step)  
 
-def fn_osr_fgsm(model, x, eps=0.05, clip_range=(None, None), return_step=False, norm_ord=torch.inf):
+
+def fn_osr_fgsm(model, x, eps=0.05, clip_range=(None, None), return_step=False, norm_ord=None):
     return fgsm(model, x, torch.zeros(len(x)), eps, norm_loss, dim=-1, ord=norm_ord, 
                 clip_range=clip_range, return_step=return_step)
 
+
 def fp_osr_fgsm_max(model, x, eps=0.05, clip_range=(None, None), return_step=False, **throw_away_args):
     return fgsm(model, x, torch.zeros(len(x)), -eps, max_loss, clip_range=clip_range, return_step=return_step)
+
 
 def fn_osr_fgsm_max(model, x, eps=0.05, clip_range=(None, None), return_step=False, **throw_away_args):
     return fgsm(model, x, torch.zeros(len(x)), eps, max_loss, clip_range=clip_range, return_step=return_step)
@@ -152,14 +213,17 @@ def fp_osr_fgsm_sum_exp(model, x, eps=0.05, clip_range=(None, None), return_step
     return fgsm(model, x, torch.zeros(len(x)), -eps, sum_exp_loss, 
                 clip_range=clip_range, return_step=return_step)
 
+
 def fn_osr_fgsm_sum_exp(model, x, eps=0.05, clip_range=(None, None), return_step=False):
     return fgsm(model, x, torch.zeros(len(x)), eps, sum_exp_loss, 
                 clip_range=clip_range, return_step=return_step)
+
 
 def fn_osr_fgsm_log_msp(model, x, eps=0.05, clip_range=(None, None), return_step=False):
     return fgsm(model, x, torch.zeros(len(x)), eps, log_msp_loss, 
                 clip_range=clip_range, return_step=return_step)
 
+# Used to save the gradient norm for an input for a model and a function
 def save_grad_norms(loss_func, model, dataloader, logdir, device, split_num, **norm_kwargs):
     model = model.to(device)
     grad_norms = []
--- our_modules/plot_tools.py
+++ our_modules/plot_tools.py
@@ -15,11 +15,16 @@ from our_modules.eval_tools import balance_binary
 from osr_closed_set_all_you_need_main.data.open_set_splits.osr_splits import osr_splits
 
 def plot_roc(ax, roc_stats, **plt_kwargs):
-    fprs, tprs, thresholds = roc_stats
+    """
+    Plots the roc cruve on a given ax given the scoring values roc_stats"""
+    fprs, tprs, _ = roc_stats
     ax.plot(fprs, tprs, **plt_kwargs)
 
 
+# Plots of images from the dataset or images with pertubations
+
 def plot_image_on_ax(ax, normalized_img, mean, std, channel=None, **plt_kwargs):
+
     normalized_img = normalized_img.to('cpu')
     c = None
     if normalized_img.requires_grad:
@@ -54,6 +59,8 @@ def plot_image_i(i, dataset, mean, std, save_path=False, **plt_kwargs):
     plt.show()
 
 
+# Class that creates compares the differences of the advesarial attacks with different epsilon values.
+
 class EpsExperimentPlot():
     def __init__(self, eps_figsize=(10,4), adv_fisize=(15,6), which_lines='both', add_zoom=(-0.003, 0.012, 0.825, 0.84), mls_title='MLS', loc_zoom = 5):
         self.which_lines = which_lines
@@ -78,6 +85,7 @@ class EpsExperimentPlot():
         self.roc_stats = None
         self.median_scores = None
 
+
     def load_mls_stats(self, path_to_eps_dirs, split_num, balance=True, dataset_name='tinyimagenet', max_eps=None, msp=False):
         self.eps, self.roc_stats, self.median_scores = load_and_eval_mls_osr_for_all_eps(path_to_eps_dirs, split_num, dataset_name=dataset_name, balance=balance, return_avg_mls=True, msp=msp)
         if max_eps is not None:
@@ -88,6 +96,7 @@ class EpsExperimentPlot():
     def load_logit_change_stats(self, path_to_eps_dirs, path_to_plain_logit_file, split_num, similarity_func=lambda after, before: torch.amax(after, dim=-1) - torch.amax(before, dim=-1), balance=True, dataset_name='tinyimagenet'):
         self.eps, self.roc_stats, self.median_scores = load_and_eval_logit_change_scores_for_all_eps(path_to_eps_dirs, path_to_plain_logit_file, split_num, similarity_func=similarity_func, dataset_name=dataset_name, balance=balance, return_avg_score=True)
    
+
     def add_to_eps_plot(self, label_suffix='', **plt_kwargs):
         aurocs = [x[1] for x in self.roc_stats]
         self.recent_eps = np.array(self.eps)
@@ -111,10 +120,12 @@ class EpsExperimentPlot():
             self.axins.set_ylim(self.add_zoom[2], self.add_zoom[3])
             mark_inset(self.ax1, self.axins, loc1=2, loc2=3, fc="none", ec="0.5")
 
+
     def load_and_add_mls_to_eps_plot(self, path_to_eps_dirs, split_num, balance=True, label_suffix='', 
                                      dataset_name='tinyimagenet', max_eps=None, invert_auroc=False,msp=False, **plt_kwargs):
         self.load_mls_stats(path_to_eps_dirs, split_num, balance=balance, dataset_name=dataset_name, max_eps=max_eps, msp=msp)
         self.add_to_eps_plot(label_suffix=label_suffix, **plt_kwargs)
+
 
     def load_and_add_logit_change_to_eps_plot(self, path_to_eps_dirs, path_to_plain_logit_file, split_num, 
                                               similarity_func=lambda after, before: torch.amax(after, dim=-1) - torch.amax(before, dim=-1), 
@@ -124,6 +135,7 @@ class EpsExperimentPlot():
         if invert_auroc:
             self.roc_stats = [(x[0], x[1]) if x[1] > 0.5 else (x[0], (1-x[1])) for x in self.roc_stats]
         self.add_to_eps_plot(label_suffix, **plt_kwargs)
+
 
     def set_legend_and_highlight_eps(self, eps_idxs=[], legend_loc=(0.72,0.8), h_line=False, label_h='MLS AUROC on $\mathcal{D}_{test}$'):
         added_legend = False
@@ -149,10 +161,14 @@ class EpsExperimentPlot():
         plt.xticks([round(l, 2) for l in locs])
         self.fig.legend(loc=legend_loc)
 
+
     def show_and_save(self, save_path=False):
         if save_path:
             plt.savefig(save_path + '.pdf', bbox_inches='tight')
         plt.show()
+
+
+# Plots that compare the differences of advesarial attacks influense on familiar or novel inputs.
 
 class IdOodPlot():
     def __init__(self):
@@ -162,7 +178,8 @@ class IdOodPlot():
         self.id_xs = None
         self.ood_ys = None
         self.ood_xs = None
-    
+
+
     def load_grad_norm_stats(self, path_grad_norms, path_plain_logits, split_num, dataset_name='tinyimagenet', score_func=lambda x:torch.amax(x, dim=-1), balance=True):
         id_stats, ood_stats = get_grad_norm_stats(path_grad_norms, path_plain_logits, split_num, dataset_name, score_func)
         id_stats = sorted(id_stats, key=lambda x: x[1])
@@ -174,6 +191,7 @@ class IdOodPlot():
         self.id_xs = [s for _, s in id_stats]
         self.ood_ys = [gn for gn, _ in ood_stats]
         self.ood_xs = [s for _, s in ood_stats]
+
 
     def load_mls_diffs_stats(self, path_plain_logits, path_fn_logits, path_csr_targets, split_num, dataset_name='tinyimagenet', balance=True):
         id_stats, ood_stats = max_logit_change_compared_id_vs_ood(path_plain_logits, path_fn_logits, path_csr_targets, split_num, dataset_name=dataset_name)
@@ -187,6 +205,7 @@ class IdOodPlot():
         self.ood_ys = [gn for gn, _ in ood_stats]
         self.ood_xs = [s for _, s in ood_stats]
 
+
     def make_boxplot(self, figsize=(6,6), ylabel=False,xlabel=False, **boxplot_kwargs):
         self.fig, self.ax = plt.subplots(1,1, figsize=figsize)
         self.ax.boxplot((self.id_ys, self.ood_ys), **boxplot_kwargs)
@@ -194,6 +213,7 @@ class IdOodPlot():
             self.ax.set_xlabel(xlabel)
         if ylabel:
             self.ax.set_ylabel(ylabel)
+
 
     def make_scatter_plot(self, window_size=5,figsize=(30,6), xlabel=r'MLS of Unmodified Images', ylabel=r'Gradient Norm - $\mathbb{E}\:\left[||\:\nabla_{\bf{x}} \log S_{\hat{y}}({\bf{x}}) \:||_1 \mid \mathcal{S}\:\right]$'):
         self.fig, self.ax = plt.subplots(1,1, figsize=figsize)
@@ -210,9 +230,10 @@ class IdOodPlot():
         self.ax.set_xlabel(xlabel)
         self.ax.set_ylabel(ylabel)
 
-    # def make_conditional_mean_plot(self, figsize=(6,6), **plot_kwargs):
+
     def set_legend(self):
         self.ax.legend()
+
 
     def show_and_save(self, save_path=False):
         if save_path:
@@ -238,6 +259,7 @@ def plot_diff_stats_for_eps(path_plain_logits, path_to_attack_folder, path_csr_t
         plt.savefig(save_path + '.pdf', transparent=True, bbox_inches='tight')
     plt.show()
 
+
 def plot_adv_imgs(eps, adv_imgs, adv_steps, mean, std, figsize=(15,10), save_path=None):
     img_stack = torch.vstack((adv_imgs[None], adv_steps[None]))
     num_imgs = len(adv_imgs)
@@ -255,6 +277,7 @@ def plot_adv_imgs(eps, adv_imgs, adv_steps, mean, std, figsize=(15,10), save_pat
         plt.savefig(save_path + '.pdf', bbox_inches='tight')
     plt.show()
 
+
 def plot_ranked_scores(path_to_logits, path_to_csr_targets, score_func=lambda ls: torch.amax(ls, dim=-1), dataset_name='tinyimagenet', 
                        split_num=0, balance=True, figsize=(6,6), step=10, s=2, ylim=None, highlight_idx = [], highlighted_label = [], highlighted_linestyle = [], ylabel='MLS', 
                        xlabel='Ranking', save_path=None, show_plain=False, show_plain_original=False, path_to_plain=''):
@@ -263,18 +286,19 @@ def plot_ranked_scores(path_to_logits, path_to_csr_targets, score_func=lambda ls
     osr_targets = get_osr_targets(csr_targets, split)
     logits = torch.load(path_to_logits)
     osr_scores = score_func(logits)
+
     if show_plain or show_plain_original:
         plain_logits = torch.load(path_to_plain)
         osr_scores_plain = score_func(plain_logits)
     else:
         osr_scores_plain = osr_scores
     highlighted_osr_scores = [osr_scores[idx] for idx in highlight_idx]
+
     if balance:
         score_label_zipped = balance_binary(zip(osr_scores.tolist(), osr_targets.tolist(), osr_scores_plain.tolist()), lambda x: bool(x[1]))
         osr_scores, osr_targets, osr_scores_plain = (torch.tensor([mls for mls, _, _ in score_label_zipped ]), 
                                        [osr_target for _, osr_target, _ in score_label_zipped], torch.tensor([plain for _,_,plain in score_label_zipped]))
 
-        
     sorted_by_score = list(enumerate(sorted((zip(osr_scores, osr_targets, osr_scores_plain)), key=lambda x: x[0])))[::step]
     id = [(i, x[0], x[2]) for i, x in sorted_by_score if not x[1]]
     ood = [(i, x[0], x[2]) for i, x in sorted_by_score if x[1]]
@@ -309,6 +333,8 @@ def plot_ranked_scores(path_to_logits, path_to_csr_targets, score_func=lambda ls
     plt.show()
 
 
+# Used for testing with median. But we ended up only using median.
+
 class EpsExperimentPlotMedian():
     def __init__(self, eps_figsize=(10,4), adv_fisize=(15,6), which_lines='both', add_zoom=(-0.003, 0.012, 0.825, 0.84), mls_title='MLS'):
         self.which_lines = which_lines
@@ -333,6 +359,7 @@ class EpsExperimentPlotMedian():
         self.roc_stats = None
         self.avg_scores = None
 
+
     def load_mls_stats(self, path_to_eps_dirs, split_num, balance=True, dataset_name='tinyimagenet', max_eps=None):
         self.eps, self.roc_stats, self.avg_scores = load_and_eval_mls_osr_for_all_eps(path_to_eps_dirs, split_num, dataset_name=dataset_name, balance=balance, return_avg_mls=True, return_quantiles=True )
         if max_eps is not None:
@@ -343,6 +370,7 @@ class EpsExperimentPlotMedian():
     def load_logit_change_stats(self, path_to_eps_dirs, path_to_plain_logit_file, split_num, similarity_func=lambda after, before: torch.amax(after, dim=-1) - torch.amax(before, dim=-1), balance=True, dataset_name='tinyimagenet'):
         self.eps, self.roc_stats, self.avg_scores = load_and_eval_logit_change_scores_for_all_eps(path_to_eps_dirs, path_to_plain_logit_file, split_num, similarity_func=similarity_func, dataset_name=dataset_name, balance=balance, return_avg_score=True)
    
+
     def add_to_eps_plot(self, label_suffix='', **plt_kwargs):
         self.avg_scores = np.transpose(np.stack(self.avg_scores, axis = 0))
         aurocs = [x[1] for x in self.roc_stats]
@@ -370,10 +398,12 @@ class EpsExperimentPlotMedian():
             self.axins.set_ylim(self.add_zoom[2], self.add_zoom[3])
             mark_inset(self.ax1, self.axins, loc1=2, loc2=3, fc="none", ec="0.5")
 
+
     def load_and_add_mls_to_eps_plot(self, path_to_eps_dirs, split_num, balance=True, label_suffix='', 
                                      dataset_name='tinyimagenet', max_eps=None, invert_auroc=False, **plt_kwargs):
         self.load_mls_stats(path_to_eps_dirs, split_num, balance=balance, dataset_name=dataset_name, max_eps=max_eps)
         self.add_to_eps_plot(label_suffix=label_suffix, **plt_kwargs)
+
 
     def load_and_add_logit_change_to_eps_plot(self, path_to_eps_dirs, path_to_plain_logit_file, split_num, 
                                               similarity_func=lambda after, before: torch.amax(after, dim=-1) - torch.amax(before, dim=-1), 
@@ -383,6 +413,7 @@ class EpsExperimentPlotMedian():
         if invert_auroc:
             self.roc_stats = [(x[0], x[1]) if x[1] > 0.5 else (x[0], (1-x[1])) for x in self.roc_stats]
         self.add_to_eps_plot(label_suffix, **plt_kwargs)
+
 
     def set_legend_and_highlight_eps(self, eps_idxs=[], legend_loc=(0.72,0.8), h_line=False, label_h='MLS AUROC'):
         added_legend = False
@@ -407,6 +438,7 @@ class EpsExperimentPlotMedian():
         locs += chosen_eps
         plt.xticks([round(l, 2) for l in locs])
         self.fig.legend(loc=legend_loc)
+
 
     def show_and_save(self, save_path=False):
         if save_path:
